145ccd7e8ee4a1b798a2900b3a313a4f97f263ce
Snehan Kekre <snehan@minerva.kgi.edu>
2017-05-22 20:35:15 -0700

    Update

--- GRAY2RGB.py
+++ GRAY2RGB.py
@@ -6,5 +6,6 @@ files = glob.glob('*.jpg')
 for file_name in range(len(files)):
 	img = cv2.imread(files[file_name],0)
 	img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-	print (files[file_name])
-	cv2.imwrite(files[file_name]+".jpg",img)
+	cv2.imwrite(files[file_name]+'.jpg',img)
+
+
